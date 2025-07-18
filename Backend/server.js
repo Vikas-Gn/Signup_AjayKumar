d0dad177e0954f85088ba396fedf114399646e5e
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-18 13:45:48 +0530

    updated-code

--- Backend/server.js
+++ Backend/server.js
@@ -228,7 +228,7 @@ app.post('/check-email-data', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3098;
-app.listen(3005, '0.0.0.0', () => {
-  console.log("Server running on port 3005");
+app.listen(3098, '0.0.0.0', () => {
+  console.log("Server running on port 3098");
   console.log(`Allowed CORS origins: ${allowedOrigins.join(', ')}`);
 });
